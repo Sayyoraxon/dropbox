eff002888458b792e8c969f754f4b84233f10d53
Student <student@iMac-Astrum-150.local>
2023-12-12 13:07:46 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Folder from './componets/Folder';
+import Home from './componets/homePage/Home';
+import LoginPage from './componets/homePage/LoginPage';
+import Main from './componets/mainPage/Main';
 
 function App() {
+
+  const [login, setLogin] = useState(localStorage.getItem("login"))
+  const [id, setId] = useState(localStorage.getItem("id"))
+
+
+  const enter = (Id) => {
+    setLogin(true)
+    localStorage.setItem("login", true)
+    setId(Id)
+    localStorage.setItem("id", Id)
+  }
+
+  
+
+
+  const routes = createBrowserRouter(
+    createRoutesFromElements(
+      <Route element={<Home login={login} />}>
+        <Route index element = {<LoginPage enter={enter} setId={setId} login={login}/>} />
+        <Route path="home" element={<Main id={id}/>}>
+        </Route>
+        <Route path="folder" element={<Folder/>}/>
+      </Route>
+    )
+  )
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={routes}/>
     </div>
   );
 }
